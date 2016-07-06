c57f8580d7e106064fe03ea8d85dbb42d4b3622d
Eric Denton <eric@iamericanartist.com>
2016-07-06 15:58:40 -0500

    got random accuracy check and random weapon powers completed

--- src/calculations.js
+++ src/calculations.js
@@ -1,7 +1,23 @@
 "use strict";
 //all damage will be calculated here
 var players = require("./player");
+// console.log("",players.player1 );
 
-// console.log("",players.player2 );
+function checkAccuracy(){
+  return Math.floor(Math.random() * (100) + 1);
+}
 
+function calculateWpnDmg (attacker, defender){
+  var randomNum = checkAccuracy();
+  if (randomNum < attacker.weapon.accuracy){
+    var randomPower = Math.floor(Math.random() * ((1.5 * attacker.weapon.power) - attacker.weapon.power) + attacker.weapon.power);
+    console.log("You Hit", randomNum);
+    console.log("Weapon Power", attacker.weapon.power);
+    console.log("Random Power", randomPower);
+  } else {
+    console.log("You Missed", randomNum);
+  }
+};
+
+calculateWpnDmg(players.player1, players.player2);
 
