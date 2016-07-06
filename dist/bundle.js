4e7f382589a1b13a2adbfeeff325cbaed0ec0e66
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-07-06 12:23:43 -0500

    template for classes, spells, and weapons completed, and able to be used with browserify

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,405 +1,75 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/*
-  Test code to generate a human player and an orc player
- */
- "use strict";
-
-var enemies = require("./enemies");
-var spells = require("./spells");
-var weapons= require("./weapons");
-var classes=require("./classes");
-var Gauntlet = require("./player");
-
-
-
-var warrior = new Gauntlet.Combatants.Human();
-warrior.setWeapon(new weapons.WarAxe());
-warrior.generateClass();  // This will be used for "Surprise me" option
-console.log(warrior.toString());
-
-var orc = new Gauntlet.Combatants.Orc();
-orc.generateClass();
-orc.setWeapon(new weapons.BroadSword());
-console.log(orc.toString());
-
-/*
-  Test code to generate a spell
- */
-var spell = new Gauntlet.SpellBook.Sphere();
-console.log("spell: ", spell.toString());
-
-
-$(document).ready(function() {
-  /*
-    Show the initial view that accepts player name
-   */
-  $("#player-setup").show();
-
-  /*
-    When any button with card__link class is clicked,
-    move on to the next view.
-   */
-  $(".card__link").click(function(e) {
-    var nextCard = $(this).attr("next");
-    var moveAlong = false;
-
-    switch (nextCard) {
-      case "card--class":
-        moveAlong = ($("#player-name").val() !== "");
-        break;
-      case "card--weapon":
-        moveAlong = ($("#player-name").val() !== "");
-        break;
-    }
-
-    if (moveAlong) {
-      $(".card").hide();
-      $("." + nextCard).show();
-    }
-  });
-
-  /*
-    When the back button clicked, move back a view
-   */
-  $(".card__back").click(function(e) {
-    var previousCard = $(this).attr("previous");
-    $(".card").hide();
-    $("." + previousCard).show();
-  });
-
-});
-
-
-},{"./classes":2,"./enemies":3,"./player":4,"./spells":5,"./weapons":6}],2:[function(require,module,exports){
-/*
-  TODO: Modularize this code with IIFE or Browserify
- */
-  "use strict";
-
-var Gauntlet = Gauntlet || {};
-Gauntlet.GuildHall = {};
-
-/*
-  Base function for a player, or enemy, class (profession)
- */
-Gauntlet.GuildHall.PlayerClass = function() {
-  this.name = "Beggar";
-  this.healthBonus = 0;
-  this.strengthBonus = 0;
-  this.intelligenceBonus = 0;
-  this.magical = false;
-
-  this.toString = function() {
-    return this.name;
-  };
-};
-
-/*
-    FIGHTER CLASSES
-      - Warrior
-      - Valkyrie
-      - Berserker
-      - Monk
- */
-Gauntlet.GuildHall.Fighter = function() {
-  this.healthBonus = 20;
-  this.strengthBonus = 10;
-};
-Gauntlet.GuildHall.Fighter.prototype = new Gauntlet.GuildHall.PlayerClass();
-
-
-Gauntlet.GuildHall.Warrior = function() {
-  this.name = "Warrior";
-  this.healthBonus = this.healthBonus + 25;
-  this.strengthBonus = this.strengthBonus + 30;
-};
-Gauntlet.GuildHall.Warrior.prototype = new Gauntlet.GuildHall.Fighter();
-
-
-Gauntlet.GuildHall.Valkyrie = function() {
-  this.name = "Valkyrie";
-  this.healthBonus = this.healthBonus + 20;
-  this.strengthBonus = this.strengthBonus + 10;
-};
-Gauntlet.GuildHall.Valkyrie.prototype = new Gauntlet.GuildHall.Fighter();
-
-
-Gauntlet.GuildHall.Berserker = function() {
-  this.name = "Berserker";
-  this.healthBonus = this.healthBonus + 35;
-  this.strengthBonus = this.strengthBonus + 20;
-};
-Gauntlet.GuildHall.Berserker.prototype = new Gauntlet.GuildHall.Fighter();
-
-
-Gauntlet.GuildHall.Monk = function() {
-  this.name = "Monk";
-  this.healthBonus = this.healthBonus + 10;
-  this.strengthBonus = this.strengthBonus + 40;
-};
-Gauntlet.GuildHall.Monk.prototype = new Gauntlet.GuildHall.Fighter();
-
-
-/*
-    MAGICAL CLASSES
-      - Shaman
-      - Wizard
-      - Conujurer
-      - Sorcerer
- */
-Gauntlet.GuildHall.Mage = function() {
-  this.name = "Mage";
-  this.magical = true;
-  this.healthBonus = this.healthBonus - 10;
-  this.strengthBonus = this.strengthBonus - 20;
-  this.intelligenceBonus = this.intelligenceBonus + 20;
-};
-Gauntlet.GuildHall.Mage.prototype = new Gauntlet.GuildHall.PlayerClass();
-
-
-Gauntlet.GuildHall.Shaman = function() {
-  this.name = "Shaman";
-  this.healthBonus = this.healthBonus + 5;
-  this.strengthBonus = this.strengthBonus - 10;
-  this.intelligenceBonus = this.intelligenceBonus + 20;
-};
-Gauntlet.GuildHall.Shaman.prototype = new Gauntlet.GuildHall.Mage();
-
-
-Gauntlet.GuildHall.Wizard = function() {
-  this.name = "Wizard";
-  this.healthBonus = this.healthBonus - 15;
-  this.strengthBonus = this.strengthBonus - 25;
-  this.intelligenceBonus = this.intelligenceBonus + 40;
-};
-Gauntlet.GuildHall.Wizard.prototype = new Gauntlet.GuildHall.Mage();
-
-
-Gauntlet.GuildHall.Conjurer = function() {
-  this.name = "Conjurer";
-  this.strengthBonus = this.strengthBonus - 10;
-  this.intelligenceBonus = this.intelligenceBonus + 10;
-};
-Gauntlet.GuildHall.Conjurer.prototype = new Gauntlet.GuildHall.Mage();
-
-
-Gauntlet.GuildHall.Sorcerer = function() {
-  this.name = "Sorcerer";
-  this.healthBonus = this.healthBonus - 5;
-  this.strengthBonus = this.strengthBonus - 20;
-  this.intelligenceBonus = this.intelligenceBonus + 30;
-};
-Gauntlet.GuildHall.Sorcerer.prototype = new Gauntlet.GuildHall.Mage();
-
-
-/*
-    STEALTH CLASSES
-      - Thief
-      - Ninja
-      - Assassin
- */
-module.exports=Gauntlet;
-
+"use strict";
+const Weapons= require("./weapons");
+const Spells= require("./spells");
+const Roles= require("./classes");
+const Players= require("./player");
+const Calculations = require("./calculations");
+},{"./calculations":2,"./classes":3,"./player":4,"./spells":5,"./weapons":6}],2:[function(require,module,exports){
+"use strict";
 },{}],3:[function(require,module,exports){
+"use strict";
+var spells = require("./spells");
+var roleList={};
+
+roleList.Employee=function(){
+  this.energy=100;
+  this.experience=100;
+  this.skill=100;
+  this.spell1=null;
+  this.spell2=null;
+
+};
+
+roleList.Intern=function(){
+  this.spell1= spells.Coffee;
+  this.energy=100;
+  
+};
+roleList.Intern.prototype = new roleList.Employee();
+
+
+var testclass = new roleList.Intern();
+console.log(testclass);
+
+module.exports=roleList;
+},{"./spells":5}],4:[function(require,module,exports){
+"use strict";
+
+
+
+},{}],5:[function(require,module,exports){
+"use strict";
+var spellList={};
+
+spellList.Spell=function(){
+  this.power=10;
+  this.lastingEffect=false;
+  this.energyRestored=0;
+};
+
+spellList.Coffee=function(){
+  this.power=0;
+  this.energyRestored=50;
+};
+
+module.exports=spellList;
+},{}],6:[function(require,module,exports){
  "use strict";
-var Gauntlet = require("./player");
+var weaponList={};
 
-Gauntlet.Combatants.Orc = function() {
-  this.health = this.health + 20;
-  this.species = "Orc";
-  this.allowedClasses = ["Warrior", "Berserker", "Shaman"];
-
-  this.generateClass = function() {
-    // Get a random index from the allowed classes array
-    var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
-
-    // Get the string at the index
-    var randomClass = this.allowedClasses[random];
-
-    // Composes the corresponding player class into the player object
-    this.class = new Gauntlet.GuildHall[randomClass]();
-    return this.class;
-  };
+weaponList.Weapon=function(){
+  this.handsUsed=1;
+  this.power=10;
+  this.accuracy=75;
 };
 
-Gauntlet.Combatants.Orc.prototype = new Gauntlet.Combatants.Monster();
-
-module.exports=Gauntlet;
-
-
-},{"./player":4}],4:[function(require,module,exports){
-/*
-  TODO: Modularize this code with IIFE or Browserify
- */
-  "use strict";
-
-var Gauntlet = require("./classes");
-Gauntlet.Combatants = {};
-
-/*
-  Define the base object for any player of Gauntlet,
-  whether a human player or a monster.
- */
-Gauntlet.Combatants.Player = function(name) {
-  this.species = null;
-  this.class = null;
-  this.weapon = null;
-
-  this.playerName = name || "unknown adventurer";
-  this.health = Math.floor(Math.random() * 40 + 50);
-  this.limbs = ["head", "neck", "arm", "leg", "torso"];
-  this.skinColor = "gray";
-  this.skinColors = [this.skinColor];
-  this.strength = 90;
-  this.intelligence = 90;
-
-  this.toString = function() {
-    var output = [this.playerName,
-      ": a ",
-      this.skinColor,
-      " skinned ",
-      this.species,
-      " ",
-      this.class,
-      " with ",
-      this.health,
-      " health. ",
-      (this.class.magical) ? "Able to cast " : " Wielding a ",
-      this.weapon.toString(),
-      "!"
-    ].join("");
-    return output;
-  };
+weaponList.laserPointer=function(){
+  this.power=5;
+  this.accuracy=100;
 };
 
-Gauntlet.Combatants.Player.prototype.setWeapon = function(newWeapon) {
-  this.weapon = newWeapon;
-};
-
-Gauntlet.Combatants.Player.prototype.generateClass = function() {
-  // Get a random index from the allowed classes array
-  var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
-
-  // Get the string at the index
-  var randomClass = this.allowedClasses[random];
-
-  // Composes the corresponding player class into the player object
-  this.class = new Gauntlet.GuildHall[randomClass]();
-
-  // Add the health bonus
-  this.health += this.class.healthBonus;
-  return this.class;
-};
-
-/*
-  Define the base properties for a human in a 
-  constructor function.
- */
-Gauntlet.Combatants.Human = function() {
-  var randomSkin;
-
-  this.species = "Human";
-  this.intelligence = this.intelligence + 20;
-
-  this.skinColors.push("brown", "red", "white", "disease");
-  randomSkin = Math.round(Math.random() * (this.skinColors.length-1));
-  this.skinColor = this.skinColors[randomSkin];
-
-  this.allowedClasses = ["Warrior", "Berserker", "Valkyrie", "Monk"];
-};
-Gauntlet.Combatants.Human.prototype = new Gauntlet.Combatants.Player();
-
-
-/*
-  Define the base properties for a monster in a 
-  constructor function.
- */
-Gauntlet.Combatants.Monster = function() {
-  this.health = this.health - 30;
-  this.intelligence = this.intelligence -20;
-  this.strength = this.strength + 30;
-};
-
-Gauntlet.Combatants.Monster.prototype = new Gauntlet.Combatants.Player();
-
-module.exports=Gauntlet;
-
-
-},{"./classes":2}],5:[function(require,module,exports){
-/*
-  TODO: Modularize this code with IIFE or Browserify
- */
-  "use strict";
-
-var Gauntlet = require("./player");
-Gauntlet.SpellBook = {};
-
-
-/*
-  Base spell function that defines name, damage, damage type
- */
-Gauntlet.SpellBook.Spell = function() {
-  this.name = "";
-  this.damage = 0;
-
-  this.damageTypes = ["lightning", "fire", "water", "earth", "mysticism"];
-  this.type = "";
-
-  this.toString = function() {
-    return this.name + " of " + this.type + " for " + this.damage + " damage!";
-  };
-};
-
-/*
-  An elemental sphere that can be cast by a magical class
- */
-Gauntlet.SpellBook.Sphere = function() {
-  this.name = "sphere";
-  this.damage = Math.floor(Math.random() * 10 + 10);
-
-  var random = Math.round(Math.random() * (this.damageTypes.length - 1));
-  this.type = this.damageTypes[random];
-};
-Gauntlet.SpellBook.Sphere.prototype = new Gauntlet.SpellBook.Spell();
-
-module.exports=Gauntlet;
-
-},{"./player":4}],6:[function(require,module,exports){
- "use strict";
-
-var Weapon = function() {
-  this.name = "bare hands";
-  this.damage = 1;
-  this.hands = 2;
-
-  this.toString = function() {
-    return this.name;
-  };
-};
-
-var Dagger = function() {
-  this.name = "dagger";
-  this.damage = 4;
-  this.hands = 1;
-};
-Dagger.prototype = new Weapon();
-
-var BroadSword = function() {
-  this.name = "broad sword";
-  this.damage = 14;
-  this.hands = 2;
-};
-BroadSword.prototype = new Weapon();
-
-var WarAxe = function() {
-  this.name = "war axe";
-  this.damage = 18;
-  this.hands = 2;
-};
-WarAxe.prototype = new Weapon();
-
-module.exports={Dagger,BroadSword,WarAxe};
-
+module.exports=weaponList;
 
 },{}]},{},[1])
 
--- src/classes.js
+++ src/classes.js
@@ -1,128 +1,25 @@
-/*
-  TODO: Modularize this code with IIFE or Browserify
- */
-  "use strict";
+"use strict";
+var spells = require("./spells");
+var roleList={};
 
-var Gauntlet = Gauntlet || {};
-Gauntlet.GuildHall = {};
+roleList.Employee=function(){
+  this.energy=100;
+  this.experience=100;
+  this.skill=100;
+  this.spell1=null;
+  this.spell2=null;
 
-/*
-  Base function for a player, or enemy, class (profession)
- */
-Gauntlet.GuildHall.PlayerClass = function() {
-  this.name = "Beggar";
-  this.healthBonus = 0;
-  this.strengthBonus = 0;
-  this.intelligenceBonus = 0;
-  this.magical = false;
-
-  this.toString = function() {
-    return this.name;
-  };
 };
 
-/*
-    FIGHTER CLASSES
-      - Warrior
-      - Valkyrie
-      - Berserker
-      - Monk
- */
-Gauntlet.GuildHall.Fighter = function() {
-  this.healthBonus = 20;
-  this.strengthBonus = 10;
+roleList.Intern=function(){
+  this.spell1= spells.Coffee;
+  this.energy=100;
+  
 };
-Gauntlet.GuildHall.Fighter.prototype = new Gauntlet.GuildHall.PlayerClass();
+roleList.Intern.prototype = new roleList.Employee();
 
 
-Gauntlet.GuildHall.Warrior = function() {
-  this.name = "Warrior";
-  this.healthBonus = this.healthBonus + 25;
-  this.strengthBonus = this.strengthBonus + 30;
-};
-Gauntlet.GuildHall.Warrior.prototype = new Gauntlet.GuildHall.Fighter();
+var testclass = new roleList.Intern();
+console.log(testclass);
 
-
-Gauntlet.GuildHall.Valkyrie = function() {
-  this.name = "Valkyrie";
-  this.healthBonus = this.healthBonus + 20;
-  this.strengthBonus = this.strengthBonus + 10;
-};
-Gauntlet.GuildHall.Valkyrie.prototype = new Gauntlet.GuildHall.Fighter();
-
-
-Gauntlet.GuildHall.Berserker = function() {
-  this.name = "Berserker";
-  this.healthBonus = this.healthBonus + 35;
-  this.strengthBonus = this.strengthBonus + 20;
-};
-Gauntlet.GuildHall.Berserker.prototype = new Gauntlet.GuildHall.Fighter();
-
-
-Gauntlet.GuildHall.Monk = function() {
-  this.name = "Monk";
-  this.healthBonus = this.healthBonus + 10;
-  this.strengthBonus = this.strengthBonus + 40;
-};
-Gauntlet.GuildHall.Monk.prototype = new Gauntlet.GuildHall.Fighter();
-
-
-/*
-    MAGICAL CLASSES
-      - Shaman
-      - Wizard
-      - Conujurer
-      - Sorcerer
- */
-Gauntlet.GuildHall.Mage = function() {
-  this.name = "Mage";
-  this.magical = true;
-  this.healthBonus = this.healthBonus - 10;
-  this.strengthBonus = this.strengthBonus - 20;
-  this.intelligenceBonus = this.intelligenceBonus + 20;
-};
-Gauntlet.GuildHall.Mage.prototype = new Gauntlet.GuildHall.PlayerClass();
-
-
-Gauntlet.GuildHall.Shaman = function() {
-  this.name = "Shaman";
-  this.healthBonus = this.healthBonus + 5;
-  this.strengthBonus = this.strengthBonus - 10;
-  this.intelligenceBonus = this.intelligenceBonus + 20;
-};
-Gauntlet.GuildHall.Shaman.prototype = new Gauntlet.GuildHall.Mage();
-
-
-Gauntlet.GuildHall.Wizard = function() {
-  this.name = "Wizard";
-  this.healthBonus = this.healthBonus - 15;
-  this.strengthBonus = this.strengthBonus - 25;
-  this.intelligenceBonus = this.intelligenceBonus + 40;
-};
-Gauntlet.GuildHall.Wizard.prototype = new Gauntlet.GuildHall.Mage();
-
-
-Gauntlet.GuildHall.Conjurer = function() {
-  this.name = "Conjurer";
-  this.strengthBonus = this.strengthBonus - 10;
-  this.intelligenceBonus = this.intelligenceBonus + 10;
-};
-Gauntlet.GuildHall.Conjurer.prototype = new Gauntlet.GuildHall.Mage();
-
-
-Gauntlet.GuildHall.Sorcerer = function() {
-  this.name = "Sorcerer";
-  this.healthBonus = this.healthBonus - 5;
-  this.strengthBonus = this.strengthBonus - 20;
-  this.intelligenceBonus = this.intelligenceBonus + 30;
-};
-Gauntlet.GuildHall.Sorcerer.prototype = new Gauntlet.GuildHall.Mage();
-
-
-/*
-    STEALTH CLASSES
-      - Thief
-      - Ninja
-      - Assassin
- */
-module.exports=Gauntlet;
+module.exports=roleList;
