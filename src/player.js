02f2464a8504dcf6f9b5f29f65890f3cea9e497d
Mike Meadows <mmeadow33@gmail.com>
2016-07-07 15:04:07 -0500

    duel weild function added

--- src/player.js
+++ src/player.js
@@ -17,14 +17,28 @@ function generateHp(player){
   return Math.floor(Math.random() * ((player.energy + 20) - player.energy) + player.energy);
 }
 
+
+function canDuelWeild (player){
+	if(player.weapon.handsUsed === 1){
+		console.log("select another weapon");
+		return true;
+	} else {
+		console.log("your hands are full");
+		return false;	
+	}
+}
+
 player1.energy =  generateHp(player1);
 // console.log("player1.energy",player1.energy);
+
+player1.weapon.canDuelWeild = canDuelWeild(player1);
+
 
 //////////////////////////// PLAYER 2
 var player2 = new Roles.Warehouse();
 player2.energy = generateHp(player2);
 player2.weapon = new Weapons.Shredder();
-
+player2.weapon.canDuelWeild = canDuelWeild(player2);
 
 
 module.exports = {player1, player2};
